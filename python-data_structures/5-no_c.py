15dfa60782f54c00af194dafcb1c2d58222c9350
sahanevalizade <shanvlizad0@gmail.com>
2025-05-06 13:09:35 +0000

    fthtfrhytfhf

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        print(" ".join("{:d}".format(num) for num in row))
+def no_c(my_string):
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
